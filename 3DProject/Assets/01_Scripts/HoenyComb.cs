665c573ec7f4cda584f7f1afa62d5187d1b058a2
minchae123 <08minchae17@gmail.com>
2023-06-12 20:29:03 +0900

    ++

--- 3DProject/Assets/01_Scripts/HoenyComb.cs
+++ 3DProject/Assets/01_Scripts/HoenyComb.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using DG.Tweening;
 
 public class HoenyComb : MonoBehaviour
 {
     public UnityEvent OnGetHoney;
+    public float moveSpeed;
+    public float power;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -13,6 +16,16 @@ public class HoenyComb : MonoBehaviour
         {
             OnGetHoney?.Invoke();
         }
+    }
+
+    private void Update()
+    {
+        Fluffy();
+    }
+
+    public void Fluffy()
+    {
+        transform.position = new Vector3(transform.position.x, Mathf.Sin(Time.time * moveSpeed) * power + power, transform.position.z);
     }
 
     public void Destroy()
